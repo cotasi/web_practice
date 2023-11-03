267ce4882c02ff53bd66545fbb7318080c2e0a36
cotasi <cotasi135@gmail.com>
2023-11-03 17:59:33 +0900

    a

--- js/web.js
+++ js/web.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
     setInterval(function(){
-            $(".swiper-wrapper").animate({"marginLeft": -1200},400,function(){
+            $(".swiper-wrapper").animate({"marginTop": -300},400,function(){
             $("div:first-child",this).appendTo($(this));
-            $(this).css("marginLeft",0)
+            $(this).css("marginTop",0)
         });
     },3000);
     
