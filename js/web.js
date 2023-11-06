c49dabbb8b8d0015cb593c30d163d7f9a0604566
cotasi <cotasi135@gmail.com>
2023-11-06 12:08:43 +0900

    페이드 슬라이드

--- js/web.js
+++ js/web.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
     // 태그객체.animate({},speed,function());
     // 태그객체.css(스타일,값)
     // 태그객체.appendTo(옮겨질객체)
-    const anih = $('.swiper-slide').height();
+    let count = 0;
 
-    setInterval(function(){
-        $('.swiper-wrapper').animate({"marginTop":-anih},400,function(){
-            $('.swiper-slide').eq(0).appendTo($('.swiper-wrapper'))
-            $('.swiper-wrapper').css("marginTop",0)
-        })
-    },3000);
+    setInterval(fadeAni,3000);
+    
+    function fadeAni() { // 선언적 함수
+        count++;
+        count = count % $('.swiper-slide').length;
+        $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on");
+    }
+
 })
